d922057ccb449fdbd04a27c48ee3a75b8f7446d3
Abdülkerem Akın <133957887+Aghapo@users.noreply.github.com>
2024-10-25 18:52:31 +0300

    Create Oyun.cs

---
+++ Oyun.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharContr : MonoBehaviour
+{
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
